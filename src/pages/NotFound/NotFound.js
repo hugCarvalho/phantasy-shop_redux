1250ecdffe48e19dffe7eb73764c8a024a0b4773
Hugo Carvalho <webnoico1978@gmail.com>
2020-10-03 23:19:18 +0200

    NotFound: basic styling and behaviour

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+
+//TODO: improve
 
 function NotFound() {
-    return (
-        <div>
-            PAGE NOT FOUND!!!
-        </div>
-    )
+  return (
+    <div className="NotFound">
+      <h1>PAGE NOT FOUND!</h1>
+      <br></br>
+      <h2>
+        Use the menu to navigate back to the website or
+        <Link to="/">
+          {" "}
+          <u>click here</u>
+        </Link>
+      </h2>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
